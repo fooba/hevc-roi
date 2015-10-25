28f59cfb5917f14ad1e6fd0433129ad680a43241
Julian <julian.thielen@gmail.com>
2015-10-25 21:05:43 +0100

    Facedet läuft sonst nix bevor auf ocv3

--- cvtest/cvtest.cpp
+++ cvtest/cvtest.cpp
@@ -28,7 +28,7 @@ void startCam(void);
 * @brief start an openCV session to the webcam and detect faces in stream
 *        quit with escape-btn
 */
-void startVid(void);
+int startVid(void);
 
 /**
 * @brief detectFaces detektiert Gesichter im uebergebenen frame.
@@ -51,9 +51,14 @@ int main(){
 
 	//imshow("Image", im);
 
-	startVid();
-	waitKey(0);
-	return 0;
+	//TODO Aenderung bzgl funktionsaufruf
+	int ret = 0;
+	//ret = startCam();
+	ret = startVid();
+	if (! ret) 
+		waitKey(0);
+	cin.ignore();
+	return ret;
 }
 
 void startCam(void){
@@ -68,9 +73,10 @@ void startCam(void){
 	//-- 2. Read the video stream
 	capture.open(0); 
 
+	
 	while (1){
 		if (capture.isOpened()){
-
+			
 			capture.read(frame);
 			if (!frame.empty()){
 				std::vector<Rect> faces = detectFaces(frame);
@@ -98,22 +104,48 @@ void startCam(void){
 //	cvReleaseCapture(&capture); //Release capture.
 }
 
-void startVid(void){
+int startVid(void){
 	cout << "clicked" << endl;
 
 	VideoCapture capture;
 	Mat frame;
 
 	//-- 1. Load the cascades
-	if (!face_cascade.load(face_cascade_name)){ cout << "--(!)Error loading: " << face_cascade_name << "\n" << endl; return; };
+	if (!face_cascade.load(face_cascade_name)){ cout << "--(!)Error loading: " << face_cascade_name << "\n" << endl; return -1; };
 
 	//-- 2. Read the video stream
-	capture.open("C:/testjb.mov");
+	capture.open("C:/test3.mov");
+	int framecounter = 0;
 
-	while (capture.isOpened()){
-		if (capture.isOpened()){
+	if (!capture.isOpened()){
+		cout << "Can't open Video..." << endl;
+		return -1;
+	}
 
-			capture.read(frame);
+	//Groesse des Input Vids
+	Size S = Size((int)capture.get(CV_CAP_PROP_FRAME_WIDTH),    // Acquire input size
+		(int)capture.get(CV_CAP_PROP_FRAME_HEIGHT));
+	double fps = capture.get(CV_CAP_PROP_FPS); //fps
+
+	//Video Output
+	VideoWriter outputVideo;
+	if (!outputVideo.open("C:/outshort.yuv", CV_FOURCC('X', '2', '6', '4'), fps, S, true)){ //out
+	//if (! outputVideo.open("C:/outshort.yuv", CV_FOURCC('H', 'E', 'V', 'C'), fps, S, true)){ //out
+	//if (! outputVideo.open("C:/outshort.yuv", CV_FOURCC('P', 'I', 'M', '1'), fps, S, true)){ //out //works but mov
+	//if (! outputVideo.open("C:/outshort.yuv", CV_FOURCC('Y', 'V', '1', '2'), fps, S, true)){ //out
+		cout << "Could not create Video writer....";
+		return -1;
+	}
+
+	if (!outputVideo.isOpened())
+	{
+		cout << "Could not open the output video to write... " << endl;
+		return -1;
+	}
+
+	while (capture.read(frame)){
+			cout << "Frame " << framecounter++ << endl;
+			
 			if (!frame.empty()){
 				std::vector<Rect> faces = detectFaces(frame);
 				for (size_t i = 0; i < faces.size(); i++) //"Male" Rechteck um jedes Gesicht
@@ -122,21 +154,15 @@ void startVid(void){
 					Point p2(faces[i].x + faces[i].width, faces[i].y + faces[i].height); //Unten rechts vom Gesicht
 					rectangle(frame, p1, p2, Scalar(255, 0, 255), 4, 8, 0); //Rechteck ums Gesicht
 				}
-				imshow(window_name, frame); //Zeige frame
+				//imshow(window_name, frame); //Zeige frame //TODO auskommentiert schneller??
+				outputVideo << frame; //Schreibe frame in output video
 			}
 			else{
-				wait4Cam--;
-				if (wait4Cam <= 0)
-					printf(" --(!) No captured frame -- :( !");
+					//break;
 			}
-		}
-		else cout << "keine Webcam ";
-		key = cvWaitKey(10);     //Capture Keyboard stroke
-		if (char(key) == 27){
-			break;      //If you hit ESC key loop will break.
-		}
 	}
 
+	return 0;
 	//	cvReleaseCapture(&capture); //Release capture.
 }
 
