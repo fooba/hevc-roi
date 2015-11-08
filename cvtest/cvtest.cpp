6a1fb57e739741bd17ec40aefcc77123d06f62c0
Julian <julian.thielen@gmail.com>
2015-11-08 20:52:40 +0100

    ffmpeg allerdings mit Speicherfehlerzugriff

--- cvtest/cvtest.cpp
+++ cvtest/cvtest.cpp
@@ -3,9 +3,21 @@
 #include <iostream>
 #include <ctime>
 
+//OpenCV
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+
+//FFMPEG (C-compiled)
+extern "C" {
+	//Library containing decoders and encoders for audio/video codecs.
+#include <libavcodec/avcodec.h>
+	//Library containing demuxers and muxers for multimedia container formats.
+#include <libavformat/avformat.h>
+	//Library performing highly optimized image scaling and color space/pixel format conversion operations.
+#include <libswscale/swscale.h>
+#include <libavutil\mem.h>
+}
 
 using namespace cv;
 using namespace std;
@@ -25,13 +37,27 @@ typedef struct{
 const std::string inputfilename = "C:\\testshort.mp4";
 const std::string outputfilename = "C:\\out.mkv";
 const std::string backgroundname   = "C:\\background.mkv";
-const int fourcc_output_codec = -1;// CV_FOURCC('P', 'I', 'M', '1');
+const int fourcc_output_codec =  CV_FOURCC('P', 'I', 'M', '1');
 std::vector<facesfromframe> facesVideo;
 
 String test;
 cv::String face_cascade_name = "haarcascade_frontalface_default.xml";
 CascadeClassifier face_cascade;
 string window_name = "Face detection & ROI encoding using HEVC";
+Mat cvFrameContext;
+struct SwsContext *pVImgConvertCtx;
+
+//fmpeg
+int videoStream; //Variable to store the index of the Video Stream.
+int frameFinished; //Variable to determine if the frame data was fetched properly
+AVFormatContext *pVFormatCtx;
+AVCodecContext *pVCodecCtx;
+AVCodec *pVCodec;
+AVFrame *pVFrame;
+AVFrame *pVFrameBGR;
+uint8_t *bufferBGR;
+AVPacket pVPacket;
+Mat inFrames;
 
 //RNG rng(12345);
 char key;
@@ -54,11 +80,18 @@ int startVid(void);
 int encodeToOne(void);
 
 /**
+* Open a Video with FFMPEG
+*/
+bool ffmpegopenVid(const char * filename);
+
+/**
 * @brief detectFaces detektiert Gesichter im uebergebenen frame.
 * @param frame Frame in dem nach gesichtern gesucht werden soll
 * @return vektor aller gefundenen Gesichter
 */
 vector<Rect> detectFaces(Mat frame);
+
+bool querryFrame(void);
 
 
 int main(){
@@ -66,9 +99,27 @@ int main(){
 	clock_t start, afterFacedet; 
 	float elapsed, elapsedafterfacedet;
 
+	av_register_all();
+
 	start = clock();
 
+	//pVFormatCtx = new AVFormatContext();
+	//pVCodecCtx = new AVCodecContext();
+	//pVCodec = new AVCodec();
+	//pVFrame = new AVFrame();
+	//pVFrameBGR = new AVFrame();
+	//bufferBGR = new uint8_t();
+
 	int ret = 0;
+	ffmpegopenVid(inputfilename.c_str());
+	int i = 0;
+	while (querryFrame()){
+		cout << "frame " << ++i << endl;
+		cout << "context: " << (int)cvFrameContext.data << endl;
+		//querryFrame();
+		cv::imshow("test", inFrames);
+	}
+/*
 	//ret = startCam();
 	ret = startVid();
 	if (ret != 0){
@@ -87,7 +138,7 @@ int main(){
 	}
 	elapsed = (float)(clock() - start) / CLOCKS_PER_SEC;
 	cout << "Time for Facedetection: " << elapsedafterfacedet << " Time for Encoding together: " << elapsed << " Sum: " << elapsedafterfacedet + elapsed << endl;
-
+*/
 	cin.ignore();
 	return ret;
 }
@@ -145,7 +196,6 @@ int startVid(void){
 	if (!face_cascade.load(face_cascade_name)){ cout << "--(!)Error loading: " << face_cascade_name << "\n" << endl; return -1; };
 
 	//-- 2. Read the video stream
-	//capture.open("D:/Users/Julian.T - mobile/..Studium/masys/projekt/samples/SpreedMovie-640x360.mkv");
 	capture.open(inputfilename);
 	int framecounter = 0;
 
@@ -181,7 +231,7 @@ int startVid(void){
 	while (capture.read(frame)){
 		cout << "Frame " << framecounter++ << endl;
 
-		//if (framecounter >= 50) break; //TODO ONLY READ first 10 FRAMES
+		if (framecounter >= 50) break; //TODO ONLY READ first 10 FRAMES
 
 		if (!frame.empty()){
 			std::vector<Rect> faces = detectFaces(frame);
@@ -293,4 +343,106 @@ int encodeToOne(void){
 
 	cout << "Ready :D" << endl;
 	return 0;
+}
+
+bool ffmpegopenVid(const char * filename){
+	bool errorStatus = false;
+
+	//openVideo File
+	if (avformat_open_input(&pVFormatCtx, filename, NULL, NULL) != 0){
+		cout << "ERROR:openVideoFile:Could not open the video file " <<filename<< endl;
+		return errorStatus = true;
+	}
+
+	// Retrieve stream information. Populates pVFormatCtx->streams with the proper information.
+	if (avformat_find_stream_info(pVFormatCtx, NULL) < 0){
+		cout << "ERROR:openVideoFile:Could not open the stream" << endl;
+	}
+
+	av_dump_format(pVFormatCtx, 0, filename, 0);
+
+	for (int i = 0; i < pVFormatCtx->nb_streams; i++){
+		if (pVFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO){
+			videoStream = i;
+			break;
+		}
+	}
+
+	if (videoStream < 0){
+		cout << "ERROR: Could not find a valid codec..." << endl;
+		return errorStatus = true;
+	}
+
+	//Get a pointer to the codec context for the video stream. The stream's information about the codec is in what we call the "codec context." This contains all the information about the codec that the stream is using, and now we have a pointer to it.
+	pVCodecCtx = pVFormatCtx->streams[videoStream]->codec;
+
+	// Find the actual codec and open it. Find the decoder for the video stream.
+	pVCodec = avcodec_find_decoder(pVCodecCtx->codec_id);
+
+	if (pVCodec == NULL){
+		cout << "ERROR:openVideoFile:Unsupported codec or codec not found!" << endl;
+		return errorStatus = true;
+	}
+
+	cout << "openVideoFile:Decoder: " << pVCodec->name << endl;
+
+	if (avcodec_open2(pVCodecCtx, pVCodec, NULL) < 0){
+		cout << "ERROR:openVideoFile : Could not open codec!" << endl;
+		return errorStatus = true;
+	}
+
+	//allocate Memory Space
+	pVFrame = av_frame_alloc();
+	pVFrameBGR = av_frame_alloc();
+
+	if (pVFrameBGR == NULL){
+		cout << "ERROR:openVideoFile:Could Not Allocate the frame!" << endl;
+		return errorStatus = true;
+	}
+
+	int numBytes = avpicture_get_size(AV_PIX_FMT_BGR24, pVCodecCtx->width, pVCodecCtx->height);
+	bufferBGR = (uint8_t *)av_mallocz(numBytes*sizeof(uint8_t));
+	avpicture_fill((AVPicture*)pVFrameBGR, bufferBGR, AV_PIX_FMT_BGR24, pVCodecCtx->width, pVCodecCtx->height);
+
+	inFrames.create(pVCodecCtx->height, pVCodecCtx->width, CV_8UC(3));
+	return errorStatus;
+}
+
+bool querryFrame(void){
+	cout << "querryFrame" << endl;
+	if (av_read_frame(pVFormatCtx, &pVPacket) < 0){
+		cout << "ERROR:queryFram:Could nor read frame!" << endl;
+		return false;
+	}
+
+	if (pVPacket.stream_index == videoStream){
+		cout << "is Stream" << pVPacket.stream_index << endl;
+		if (avcodec_decode_video2(pVCodecCtx, pVFrame, &frameFinished, &pVPacket) < 0){
+			cout << "Error:querryFrame:Could not decode Video!" << endl;
+			return false;
+		}
+
+		if (frameFinished){
+			cout << "frame finished" << endl;
+			if (pVImgConvertCtx == NULL){
+				pVImgConvertCtx = sws_getContext(pVCodecCtx->width, pVCodecCtx->height, pVCodecCtx->pix_fmt, pVCodecCtx->width, pVCodecCtx->height, AV_PIX_FMT_BGR24, SWS_BICUBIC, NULL, NULL, NULL);
+			}
+			if (pVImgConvertCtx == NULL){
+				cout << "ERROR:queryFrame:Cannot initialize the conversation context!" << endl;
+				return false;
+			}
+			
+			sws_scale(pVImgConvertCtx, pVFrame->data, pVFrame->linesize, 0, pVCodecCtx->height, pVFrameBGR->data, pVFrameBGR->linesize);
+
+			//OpenCV
+			for (int y = 0; y < pVCodecCtx->height; y++){
+				for (int x = 0; x < pVCodecCtx->width; x++){
+					cvFrameContext.at<cv::Vec3b>(y, x)[0] = pVFrameBGR->data[0][y * pVFrameBGR->linesize[0] + x * 3 + 0];
+					cvFrameContext.at<cv::Vec3b>(y, x)[1] = pVFrameBGR->data[0][y * pVFrameBGR->linesize[0] + x * 3 + 1];
+					cvFrameContext.at<cv::Vec3b>(y, x)[2] = pVFrameBGR->data[0][y * pVFrameBGR->linesize[0] + x * 3 + 2];
+				}
+			}
+		}
+	}
+	return true;
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+Facedetection with OpenCV Convert to ROI for HEVC Encoding
+==========================================================
+
+This project should be detect faces in Videos and encode the detected ROI with a lower Quantization than the background.
